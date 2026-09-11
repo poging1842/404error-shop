505418fb9e145e5156cc6d08008a51219749006f
poging1842 <jeicenj@gmail.com>
2026-09-11 14:50:21 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,93 @@
+import os
+import handlers
+from aiogram import executor, types
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from data import config
+from loader import dp, db, bot
+import filters
+import logging
+
+filters.setup(dp)
+
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get("PORT", 5000))
+user_message = 'User'
+admin_message = 'Admin'
+
+
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    markup.row(user_message, admin_message)
+
+    await message.answer('''Hello! 👋
+
+🤖 I am an e-commerce shop bot for selling products of any category.
+    
+🛍️ To go to the catalog and choose items you like, use the /menu command.
+
+💰 You can top up your balance via YooKassa (Yandex.Checkout), Sberbank, or Qiwi.
+
+❓ Have questions? No problem! The /sos command will help you contact the admins, who will try to respond as quickly as possible.
+
+🤝 Want to order a similar bot? Contact the developer <a href="https://t.me/NikolaySimakov">Nikolay Simakov</a>, he doesn't bite)))
+    ''', reply_markup=markup)
+
+
+@dp.message_handler(text=user_message)
+async def user_mode(message: types.Message):
+
+    cid = message.chat.id
+    if cid in config.ADMINS:
+        config.ADMINS.remove(cid)
+
+    await message.answer('User mode enabled.', reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(text=admin_message)
+async def admin_mode(message: types.Message):
+
+    cid = message.chat.id
+    if cid not in config.ADMINS:
+        config.ADMINS.append(cid)
+
+    await message.answer('Admin mode enabled.', reply_markup=ReplyKeyboardRemove())
+
+
+async def on_startup(dp):
+    logging.basicConfig(level=logging.INFO)
+    db.create_tables()
+
+    await bot.delete_webhook()
+    if config.WEBHOOK_URL:
+        await bot.set_webhook(config.WEBHOOK_URL)
+
+
+async def on_shutdown():
+    logging.warning("Shutting down..")
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+    logging.warning("Bot down")
+
+
+if __name__ == '__main__':
+
+    if (("HEROKU_APP_NAME" in list(os.environ.keys())) or
+        ("RAILWAY_PUBLIC_DOMAIN" in list(os.environ.keys()))):
+
+        executor.start_webhook(
+            dispatcher=dp,
+            webhook_path=config.WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+        )
+
+    else:
+
+        executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
