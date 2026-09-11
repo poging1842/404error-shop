00f75f41a72b3ba6ce383afa53c8231d4af22084
poging1842 <jeicenj@gmail.com>
2026-09-11 15:01:24 +0800

    Add files via upload

---
+++ handlers/user/menu.py
@@ -0,0 +1,29 @@
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardMarkup
+from loader import dp
+from filters import IsAdmin, IsUser
+
+catalog = '🛍️ Catalog'
+balance = '💰 Balance'
+cart = '🛒 Cart'
+delivery_status = '🚚 Delivery status'
+
+settings = '⚙️ Catalog settings'
+orders = '🚚 Orders'
+questions = '❓ Questions'
+
+@dp.message_handler(IsAdmin(), commands='menu')
+async def admin_menu(message: Message):
+    markup = ReplyKeyboardMarkup(selective=True)
+    markup.add(settings)
+    markup.add(questions, orders)
+
+    await message.answer('Menu', reply_markup=markup)
+
+@dp.message_handler(IsUser(), commands='menu')
+async def user_menu(message: Message):
+    markup = ReplyKeyboardMarkup(selective=True)
+    markup.add(catalog)
+    markup.add(balance, cart)
+    markup.add(delivery_status)
+
+    await message.answer('Menu', reply_markup=markup)
