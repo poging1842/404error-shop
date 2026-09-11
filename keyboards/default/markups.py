bed4f2bdfb445f5f172fcb0f8d4fd9df36fed59d
poging1842 <jeicenj@gmail.com>
2026-09-11 15:05:13 +0800

    Add keyboard markup functions for bot responses

---
+++ keyboards/default/markups.py
@@ -0,0 +1,27 @@
+from aiogram.types import ReplyKeyboardMarkup
+
+back_message = '👈 Back'
+confirm_message = '✅ Confirm order'
+all_right_message = '✅ All right'
+cancel_message = '🚫 Cancel'
+
+def confirm_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(confirm_message)
+    markup.add(back_message)
+    return markup
+
+def back_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(back_message)
+    return markup
+
+def check_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(back_message, all_right_message)
+    return markup
+
+def submit_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(cancel_message, all_right_message)
+    return markup
